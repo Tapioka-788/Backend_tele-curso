c2d5cd216c4dd888f73abadaee2fb5dc8a4cd1d1
Tapioka-788 <guilhermezuchellifelipetto@gmail.com>
2024-10-09 17:24:58 -0300

    adiciona, deleta e edita em jason

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// configuracoes do ip 172.17.16.59
+
 const cors = require('cors');
 const express = require('express');
 
@@ -7,33 +9,63 @@ const porta = 3000;
 app.use(cors());
 app.use(express.json());
 
-// Dados do cartao
+// Variaveis
 
-let cartoes = [
+const cartoes = [
     {
-        salario: 'salario 10' ,
-        img: ''},
+        salario: 'salario 10',
+        numero: '0'
+    },
     {
-        salario: 'salario 11' ,
-        img: ''},
+        salario: 'salario 11',
+        numero: '1'
+    },
     {
-        salario: 'salario 12' ,
-        img: ''},
+        salario: 'salario 12',
+        numero: '2'
+    },
     {
-        salario: 'salario 13' ,
-        img: ''},
+        salario: 'salario 13',
+        numero: '3'
+    },
     {
-        salario: 'salario 14' ,
-        img: ''},
+        salario: 'salario 14',
+        numero: '4'
+    },
     {
-        salario: 'salario 15' ,
-        img: ''}
+        salario: 'salario 15',
+        numero: '5'
+    }
 ]
 
-app.get('/api/cartoes', (req, res) => {
+// Caminhos
+
+app.get('/salario', (req, res) => {
     res.status(200).json(cartoes)
     console.log('oi')
 })
+
+app.post('/salario', (req, res) => {
+    const salario = req.body.salario
+    cartoes.push({salario:salario, cartoes: cartoes.length})
+    console.log(cartoes)
+    res.status(201).json({ salario: 'Saralio novo' })
+})    
+
+app.delete('/salario', (req, res) => {
+    const numero = req.body.numero
+    cartoes.splice(numero, 1)
+    console.log(cartoes)
+    res.status(201).json({ salario: 'Saralio excluido' })
+})  
+
+app.put('/salario', (req, res) => {
+    const numero = req.body.numero
+    const salario = req.body.salario
+    cartoes[numero].salario = salario
+    console.log(cartoes)
+    res.status(201).json({ salario: 'Saralio atualizado' })
+})  
 
 app.listen(porta, () => {
     console.log(`Servidor rodando na porta ${porta}`);
