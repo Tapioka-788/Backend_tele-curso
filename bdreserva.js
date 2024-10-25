60eecae06f08be8516bb3de25b353a5a7a3662a6
Tapioka-788 <guilhermezuchellifelipetto@gmail.com>
2024-10-25 17:12:44 -0300

    start do Bd

---
+++ bdreserva.js
@@ -0,0 +1,38 @@
+
+// const cartoes = [
+//     {
+//         salario: 'salario 11',
+//         vaga: 'pizzaiolo',
+//         descricao: 'a vaga esta vaga',
+//         horario: 'horas para dar',
+//         nome: 'salamidas',
+//     },
+//     {
+//         salario: 'salario 12',
+//         vaga: 'padeiro',
+//         descricao: 'a vaga esta vaga',
+//         horario: 'horas para dar',
+//         nome: 'salamidas',
+//     },
+//     {
+//         salario: 'salario 13',
+//         vaga: 'padre',
+//         descricao: 'a vaga esta vaga',
+//         horario: 'horas para dar',
+//         nome: 'salamidas',
+//     },
+//     {
+//         salario: 'salario 14',
+//         vaga: 'acogueiro',
+//         descricao: 'a vaga esta vaga',
+//         horario: 'horas para dar',
+//         nome: 'salamidas',
+//     },
+//     {
+//         salario: 'salario 15',
+//         vaga: 'dev web',
+//         descricao: 'a vaga esta vaga',
+//         horario: 'horas para dar',
+//         nome: 'salamidas',
+//     }
+// ]
--- index.js
+++ index.js
@@ -2,6 +2,15 @@
 
 const cors = require('cors');
 const express = require('express');
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./chavefirebase.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
+
+const bd = admin.firestore();
 
 const app = express();
 const porta = 3000;
@@ -9,79 +18,74 @@ const porta = 3000;
 app.use(cors());
 app.use(express.json());
 
-// Variaveis
-
-const cartoes = [
-    {
-        salario: 'salario 10',
-        vaga: 'pedreiro',
-        descricao: 'a vaga esta vaga',
-        horario: 'horas para dar',
-        nome: 'salamidas',
-    },
-    {
-        salario: 'salario 11',
-        vaga: 'pizzaiolo',
-        descricao: 'a vaga esta vaga',
-        horario: 'horas para dar',
-        nome: 'salamidas',
-    },
-    {
-        salario: 'salario 12',
-        vaga: 'padeiro',
-        descricao: 'a vaga esta vaga',
-        horario: 'horas para dar',
-        nome: 'salamidas',
-    },
-    {
-        salario: 'salario 13',
-        vaga: 'padre',
-        descricao: 'a vaga esta vaga',
-        horario: 'horas para dar',
-        nome: 'salamidas',
-    },
-    {
-        salario: 'salario 14',
-        vaga: 'acogueiro',
-        descricao: 'a vaga esta vaga',
-        horario: 'horas para dar',
-        nome: 'salamidas',
-    },
-    {
-        salario: 'salario 15',
-        vaga: 'dev web',
-        descricao: 'a vaga esta vaga',
-        horario: 'horas para dar',
-        nome: 'salamidas',
-    }
-]
-
 // Caminhos
 
-app.get('/salario', (req, res) => {
-    res.status(200).json({ cartoes })
+app.get('/salario', async (req, res) => {
+    try {
+        const response = await bd.collection('cartoes').get();
+        const cartoes = response.docs.map(doc => ({
+            id: doc.id, ...doc.data(),
+        }));
+        res.status(200).json({ cartoes });
+    } catch {
+        console.log(e);
+    }
+
     console.log('oi')
 })
 
-app.post('/salario', (req, res) => {
-    const { salario, vaga, descricao, horario, nome } = req.body
+app.post('/salario', async (req, res) => {
+    try {
+        const { salario, vaga, descricao, horario, nome } = req.body
+        const novoDoc = await bd.collection('cartoes').add({
+            salario: salario,
+            vaga: vaga,
+            descricao: descricao,
+            horario: horario,
+            nome: nome
+        })
 
-    cartoes.push({ salario: salario, vaga: vaga, descricao: descricao, horario: horario, nome: nome })
-    console.log(cartoes)
-    res.status(201).json({ mensagem: 'Saralio novo' })
+        console.log(cartoes)
+        res.status(201).json({ mensagem: 'Saralio novo' })
+    }
+    catch { }
+
 })
 
-app.delete('/salario', (req, res) => {
-    const { cartao } = req.body
 
-    cartoes.splice(cartao, 1)
-    console.log(cartao + 'deletado')
-    res.status(201).json({ mensagem: 'Saralio excluido' + cartao })
-})
+// app.post('/salario', (req, res) => {
+//     const { salario, vaga, descricao, horario, nome } = req.body
+
+//     cartoes.push({ salario: salario, vaga: vaga, descricao: descricao, horario: horario, nome: nome })
+//     console.log(cartoes)
+//     res.status(201).json({ mensagem: 'Saralio novo' })
+// })
+
+app.delete('/salario', async (req, res) => {
+    try {
+        const { cartao } = req.body;
+        const response = await bd.collection('cartoes').doc(id).delete(id);
+        console.log(`Cartão com ID ${id} deletado com sucesso.`);
+        res.status(200).json({ message: `Cartão com ID ${id} deletado com sucesso.` });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ message: "Erro ao deletar o cartão." });
+    }
+    console.log('oi')
+});
+
+
+// app.delete('/salario', (req, res) => {
+//     const { cartao } = req.body
+
+//     cartoes.splice(cartao, 1)
+//     console.log(cartao + 'deletado')
+//     res.status(201).json({ mensagem: 'Saralio excluido' + cartao })
+// })
 
 app.put('/salario', (req, res) => {
-    const { salario, vaga, descricao, horario, id, nome} = req.body;
-    cartoes[id] = { salario: salario, vaga: vaga, descricao: descricao, horario: horario, nome: nome,}
+    const { salario, vaga, descricao, horario, id, nome } = req.body;
+    cartoes[id] = { salario: salario, vaga: vaga, descricao: descricao, horario: horario, nome: nome, }
 
     console.log('Atulazido')
     console.log(cartoes);
