a425f40bf0fb726d0cff8c7ac2f7f47763fbd182
Tapioka-788 <guilhermezuchellifelipetto@gmail.com>
2024-10-30 17:26:37 -0300

    aaaaaaaaaa

--- api/index.js
+++ api/index.js
@@ -112,7 +112,7 @@ app.post('/salario', async (req, res) => {
 
 
 app.delete('/salario', async (req, res) => {
-    const { id } = req.body;
+    const { id } = req.body.cartao;
     if (!id) {
         res.status(400).json({ mensagem: 'Id não fornecido' });
         console.log('Id não fornecido');
